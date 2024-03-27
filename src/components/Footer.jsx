ed2281a94eafcb608943b34e1927a38cd159803d
Gregory Mjange <gregorymjange2000@gmail.com>
2024-03-27 18:25:26 +0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,6 @@ function Footer() {
             sx={{
                 // background: "radial-gradient(ellipse farthest-corner at right bottom, #FEDB37 0%, #FDB931 8%, #9f7928 30%, #8A6E2F 40%, transparent 80%),radial-gradient(ellipse farthest-corner at left top, #FEDB37 0%, #FDB931 8%, #D1B464 25%, #5d4a1f 62.5%, #5d4a1f 100%)",
                 backgroundColor: 'common.black',
-                opacity: 0.8
             }}
         >
             <Container>
